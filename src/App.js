23c504da2fcaa4d9af9cae491d3eba5bb5f6ad7c
aayushinam <aayushinamdev281@gmail.com>
2022-05-25 00:24:06 +0530

    commit for porject LiveCovidTracker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
+const App = () =>{
+    const[data,setData] = useState([]);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const getCovidData = async() => {
+    try{
+        const res = await fetch('https://data.covid19india.org/data.json');
+        const actualData = await res.json();
+        console.log(actualData.statewise[0]);
+        setData(actualData.statewise[0]);
+    } catch (error){
+        console.log(Error);
+    }
 }
+useEffect(() =>{
+    getCovidData();
+}, []);
+return(
+<>
+    <section>
+    <div className='div_live'>
+        <div id='dot'></div>
+        <h1>Live</h1>
+    </div>
 
-export default App;
+    <h2>COVID-19 CORONAVIRUS TRACKER</h2>
+    <div className='main_content'>
+        <ul className='content'>
+            <li className='card1'>
+            <div className='card_inner'>
+                <p className=' card_name'><span>Our</span> COUNTRY</p>
+                <h1 className=' card_total card_small'>INDIA</h1>
+            </div>
+            </li>
+            <li className='card2'>
+            <div className='card_inner'>
+                <p className=' card_name'><span>Total</span> RECOVERED</p>
+                <h1 className=' card_total card_small'>{data.recovered}</h1>
+            </div>
+            </li>
+            <li className='card3'>
+            <div className='card_inner'>
+                <p className=' card_name'><span>Total</span> CONFIRMED</p>
+                <h1 className=' card_total card_small'>{data.deltaconfirmed}</h1>
+            </div>
+            </li>
+            <li className='card4'>
+            <div className='card_inner'>
+                <p className=' card_name'><span>Total</span> DEATH</p>
+                <h1 className=' card_total card_small'>{data.deaths}</h1>
+            </div>
+            </li>
+            <li className='card5'>
+            <div className='card_inner'>
+                <p className=' card_name'><span>Total</span> ACTIVE</p>
+                <h1 className=' card_total card_small'>{data.active}</h1>
+            </div>
+            </li>
+            <li className='card6'>
+            <div className='card_inner'>
+                <p className=' card_name'><span>Last</span> UPDATED</p>
+                <h1 className=' card_total card_small'>{data.lastupdatedtime}</h1>
+            </div>
+            </li>
+        </ul>
+    </div>
+    </section>
+</>
+)
+}
+export default App
